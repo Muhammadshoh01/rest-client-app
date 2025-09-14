7faba6aafbbf4d76b26f69d1b233828c93af91d2
hitman46923 <al46923@outlook.com>
2025-09-14 19:10:25 +0300

    fix: fix   push

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,23 @@
+
+export function Footer () {
+    return (
+        <div className="footer-wrapper">
+            <div className="github-links">
+                <a href="https://github.com/muhammadshoh01"><img className="git-image" src="/github.png" width="50" height = '50' ></img>muhammadshoh01</a>
+                <a href="https://github.com/yelantsevv"><img className="git-image" src="/github.png" width="50" height = '50' ></img>yelantsevv</a>
+                <a href="https://github.com/hitman46923"><img className="git-image" src="/github.png" width="50" height = '50' ></img>hitman46923</a>
+            </div>
+            <div className="date">
+                2025
+            </div>
+
+                <div className="link">
+                    <a href="https://rs.school/courses/reactjs"><img src="./rss-logo.svg" width="50" height = '50' alt="" /></a>
+                </div>
+
+            
+        </div>
+    )
+
+
+}
---
+++ src/app/components/header.tsx
@@ -0,0 +1,15 @@
+
+export function Header () {
+    
+return (
+    <div className="header-wrapper">
+    <img src="/postman-dark-logo.svg" width="50" height = '50'   className="logo"></img>
+
+    <button  >Change Language</button>
+    <button >Sign In</button>
+    <button >Sign Out</button>
+    </div>
+)
+
+
+}
